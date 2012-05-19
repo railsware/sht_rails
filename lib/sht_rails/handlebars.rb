3e75bb257847ba46c260b73cdfb1552f42379801
Alexey <leopard.not.a@gmail.com>
2012-05-20 01:38:23 +0300

    fix view render

--- lib/sht_rails/handlebars.rb
+++ lib/sht_rails/handlebars.rb
@@ -6,9 +6,10 @@ module ShtRails
   module Handlebars
     def self.call(template)
       if template.locals.include?(ShtRails.action_view_key.to_s) || template.locals.include?(ShtRails.action_view_key.to_sym)
-        handlebars = Handlebars::Context.new
-        template = handlebars.compile(template.source.inspect)
-        "template.call(#{SmtRails.action_view_key.to_s}).html_safe"
+<<-SHT
+  handlebars_context = Handlebars::Context.new
+  handlebars_context.compile(#{template.source.inspect}).call(#{ShtRails.action_view_key.to_s} || {}).html_safe
+SHT
       else
         "#{template.source.inspect}.html_safe"
       end
